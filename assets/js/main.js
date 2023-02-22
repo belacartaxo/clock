6930161478858e9278576b8220e27b6a676587b7
Isabela Britto Cartaxo <111789361+belacartaxo@users.noreply.github.com>
2023-02-21 22:03:04 -0300

    completed project

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,17 @@ const hourHand = document.querySelector('.hand.hours');
 const minuteHand = document.querySelector('.hand.minutes');
 const secondHand = document.querySelector('.hand.seconds');
 
-function setRotation (element, rotation){
-    element.style.setProperty('--rotation', rotation);
+function setRotation (element, percentage){
+    element.style.setProperty('--rotation', percentage*360);
 }
 
 setInterval(()=>{
     const date = new Date();
-    const hourRotation = date.getHours()/12*360;
-    const minutesRotation = date.getMinutes()/60*360;
-    const secondsRotation = date.getSeconds()/60*360;
-    setRotation(secondHand, secondsRotation);
-    setRotation(minuteHand, minutesRotation);
-    setRotation(hourHand, hourRotation);
+    const secondsPercentage = date.getSeconds()/60;
+    const minutesPercentage = (secondsPercentage + date.getMinutes())/60;
+    const hourPercentage = (minutesPercentage + date.getHours())/12;
+
+    setRotation(secondHand, secondsPercentage);
+    setRotation(minuteHand, minutesPercentage);
+    setRotation(hourHand, hourPercentage);
 }, 1000);
